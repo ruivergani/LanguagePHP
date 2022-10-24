46f2cb23422354c8792915b6aef1ca461274052b
Rui Vergani Neto <ruiverganineto@gmail.com>
2022-10-24 14:24:23 +0100

    Update task6.php

--- week2/1. PHP and HTML/task6.php
+++ week2/1. PHP and HTML/task6.php
@@ -4,6 +4,37 @@
     $password1 = $_POST["password1"];
     $password2 = $_POST["password2"];
 ?>
+// A new way of doing the same exercise (tutorial task6)
+// if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+//     $username = (string) $_POST['username'];
+//     $password = (string) $_POST['password1'];
+//     $password_confirm = (string) $_POST['password2'];
+
+//     $username_length = count(str_split($username));
+
+//     if ($username_length < 4 || ($username_length > 15)) {
+//         $errors[] = 'Username must be between 4 and 15 characters';
+//     }
+
+//     if ($password == '' || $password_confirm == '' || $password !== $password_confirm) {
+//         $errors[] = "You must enter a password and re-enter it";
+//     }
+
+//     if ($errors === []) {
+//         $output = "Welcome!";
+//     }
+// }
+    
+<!-- 
+     <?php if (count($errors) > 0): ?>
+        <ul>
+            <?php foreach ($errors as $error): ?>
+                <li><?= $error ?></li>
+            <?php endforeach; ?>
+        </ul>
+    <?php endif; ?>
+-->
+    
 <!DOCTYPE html>
 <html lang="en">
 <head>

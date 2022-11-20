f75f8eec5d315c1a458a49513b7d4182add1ffc5
ruivergani <ruiverganineto@gmail.com>
2022-11-20 17:24:17 +0000

    Use Filesystem Class to Read all posts

--- laravel/blog/resources/views/posts.blade.php
+++ laravel/blog/resources/views/posts.blade.php
@@ -8,29 +8,10 @@
     <link rel="stylesheet" href="/app.css">
 </head>
 <body>
-    <article>
-        <h1>
-            <a href="/posts/my-first-post">My First Post</a>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea accusantium hic explicabo, ratione amet quo optio nostrum sit labore quasi eos, error sed fugiat quibusdam quas nobis sequi minima officiis?
-            </p>
-        </h1>
-    </article>
-    <article>
-        <h1>
-            <a href="/posts/my-second-post">My Second Post</a>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea accusantium hic explicabo, ratione amet quo optio nostrum sit labore quasi eos, error sed fugiat quibusdam quas nobis sequi minima officiis?
-            </p>
-        </h1>
-    </article>
-    <article>
-        <h1>
-            <a href="/posts/my-third-post">My Third Post</a>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea accusantium hic explicabo, ratione amet quo optio nostrum sit labore quasi eos, error sed fugiat quibusdam quas nobis sequi minima officiis?
-            </p>
-        </h1>
-    </article>
+    <?php foreach($posts as $post) : ?>
+        <article>
+            <?=  $post; ?>
+        </article>
+    <?php endforeach; ?>
 </body>
 </html>

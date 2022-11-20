4cccca1bd68ed0ec66b9615e8e202a17ee4cd790
ruivergani <ruiverganineto@gmail.com>
2022-11-20 13:58:59 +0000

    changes folder structure

--- laravel/laragigs/routes/web.php
+++ laravel/laragigs/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,37 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// you can use any HTTP method you want
 Route::get('/', function () {
-    return view('welcome');
+    return view('listings', [ // listing is in the views/listing.php
+        'heading' => 'Latest Listings',
+        'listings' => [
+            [
+                'id' => 1,
+                'title' => 'Listing One',
+                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Listing Two',
+                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing'
+            ]
+        ]
+    ]);
 });
+
+// Route::get('/hello', function(){
+//     return response('<h1>Hello World!</h1>', 200)
+//     ->header('Content-Type', 'text/plain')
+//     ->header('foo', 'bar');
+// });
+
+// Route::get('/posts/{id}', function($id) {
+//     //dd($id); // stop run and show the ID passed in (die dump)
+//     return response('Post ' . $id);
+// })->where('id', '[0-9]+');
+
+// Route::get('/search', function (Request $request){
+//     //dd($request);
+//     return $request->name;
+// });

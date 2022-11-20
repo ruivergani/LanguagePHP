a45561cd624399fc49f73fcb0548122da354cc59
ruivergani <ruiverganineto@gmail.com>
2022-11-20 14:50:22 +0000

    Make a Route and Link to it

---
+++ laravel/blog/resources/views/post.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Blog</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+<body>
+    <article>
+        <h1>
+            <a href="/post">My First Post</a>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea accusantium hic explicabo, ratione amet quo optio nostrum sit labore quasi eos, error sed fugiat quibusdam quas nobis sequi minima officiis?
+            </p>
+        </h1>
+    </article>
+
+    <a href="/">Go Back</a>
+
+</body>
+</html>
